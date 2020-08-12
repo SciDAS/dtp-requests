b2a4825e14e492d5bd0690da75a6f6f41394afbb
Cole McKnight <cbmckni@clemson.edu>
2020-08-12 09:01:30 -0700

    structure change

---
+++ submit.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+job=$2
+jobname=${2%.sh}
+jobyaml="${jobname}.yaml"
+dir=$1
+
+cd /workspace/dtp-jobs/$dir/$jobname
+
+case $1 in
+    dtp-aspera)
+        cat <<EOF > "$jobyaml"
+        apiVersion: batch/v1
+        kind: Job
+        metadata:
+          name: ${jobname}
+        spec:
+          template:
+            spec:
+              containers:
+              - name: ${jobname}
+                image: ibmcom/aspera-cli
+                command: [ "/bin/bash", "-c", "--" ]
+                args: [ "chmod -R 777 /workspace/dtp-jobs/${dir}/${jobname} && cd /workspace/dtp-jobs/${dir}/${jobname} && ${job}" ]
+              restartPolicy: Never
+          backoffLimit: 4
+        EOF
+        ;;
+    dtp-aws)
+        cat <<EOF > "$jobyaml"
+        apiVersion: batch/v1
+        kind: Job
+        metadata:
+          name: ${jobname}
+        spec:
+          template:
+            spec:
+              containers:
+              - name: ${jobname}
+                image: mesosphere/aws-cli
+                command: ["/bin/sh"]
+                args: ["-c", "apk update && apk upgrade && apk add bash && chmod -R 777 /workspace/dtp-jobs/${dir}/${jobname} && cd /workspace/dtp-jobs/${dir}/${jobname} && ${job}" ]
+              restartPolicy: Never
+          backoffLimit: 4
+        EOF
+        ;;
+    dtp-gcp)
+        cat <<EOF > "$jobyaml"
+        apiVersion: batch/v1
+        kind: Job
+        metadata:
+          name: ${jobname}
+        spec:
+          template:
+            spec:
+              containers:
+              - name: ${jobname}
+                image: google/cloud-sdk
+                command: [ "/bin/bash", "-c", "--" ]
+                args: [ "chmod -R 777 /workspace/dtp-jobs/${dir}/${jobname} && cd /workspace/dtp-jobs/${dir}/${jobname} && ${job}" ]
+              restartPolicy: Never
+          backoffLimit: 4
+        EOF
+        ;;
+    dtp-irods)
+        cat <<EOF > "$jobyaml"
+        apiVersion: batch/v1
+        kind: Job
+        metadata:
+          name: ${jobname}
+        spec:
+          template:
+            spec:
+              containers:
+              - name: ${jobname}
+                image: cbmckni/dtp-irods
+                command: [ "/bin/bash", "-c", "--" ]
+                args: [ "chmod -R 777 /workspace/dtp-jobs/${dir}/${jobname} && cd /workspace/dtp-jobs/${dir}/${jobname} && ${job}" ]
+              restartPolicy: Never
+          backoffLimit: 4
+        EOF
+        ;;
+    dtp-minio)
+        cat <<EOF > "$jobyaml"
+        apiVersion: batch/v1
+        kind: Job
+        metadata:
+          name: ${jobname}
+        spec:
+          template:
+            spec:
+              containers:
+              - name: ${jobname}
+                image: minio/mc
+                command: ["/bin/sh"]
+                args: ["-c", "apk update && apk upgrade && apk add bash && chmod -R 777 /workspace/dtp-jobs/${dir}/${jobname} && cd /workspace/dtp-jobs/${dir}/${jobname} && ${job}" ]
+              restartPolicy: Never
+          backoffLimit: 4
+        EOF
+        ;;
+    dtp-ndn)
+        cat <<EOF > "$jobyaml"
+        apiVersion: batch/v1
+        kind: Job
+        metadata:
+          name: ${jobname}
+        spec:
+          template:
+            spec:
+              containers:
+              - name: ${jobname}
+                image: cbmckni/ndn-tools
+                command: [ "/bin/bash", "-c", "--" ]
+                args: [ "chmod -R 777 /workspace/dtp-jobs/${dir}/${jobname} && '/usr/local/bin/nfd -c $CONFIG > $LOG_FILE 2>&1' && cd /workspace/dtp-jobs/${dir}/${jobname} && ${job}" ]
+              restartPolicy: Never
+          backoffLimit: 4
+        EOF
+        ;;
+    dtp-sra-tools)
+        cat <<EOF > "$jobyaml"
+        apiVersion: batch/v1
+        kind: Job
+        metadata:
+          name: ${jobname}
+        spec:
+          template:
+            spec:
+              containers:
+              - name: ${jobname}
+                image: ncbi/sra-tools
+                command: [ "/bin/sh" ]
+                args: ["-c", "apk update && apk upgrade && apk add bash && chmod -R 777 /workspace/dtp-jobs/${dir}/${jobname} && cd /workspace/dtp-jobs/${dir}/${jobname} && ${job}" ]
+              restartPolicy: Never
+          backoffLimit: 4
+        EOF
+        ;;
+    *)
+        echo "Protocol ${dir} not found."
+        exit 1
+        ;;
+esac
+
+kubectl create -f $jobyaml
+echo "Job ${jobname} submitted."
--- update.sh
+++ update.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 
-cd /workspace/dtp-requests/$1
-for job in *.sh ; do
-    if /bin/bash "$job" > "$job".log 2>&1 ; then
-        echo "Job succeeded" "$job"
-        mv "$job" "$job".success
-    else
-        echo "Job failed" "$job"
-        mv "$job" "$job".fail
-    fi
+git clone https://github.com/cbmckni/dtp-requests.git /workspace
+cd /workspace/dtp-requests
+for dir in */ ; do
+    mkdir -p /workspace/dtp-jobs/$dir
+    cd /workspace/dtp-requests/$dir
+    for job in *.sh ; do
+        jobname=${job%.sh}
+        mkdir /workspace/dtp-jobs/$dir/$jobname || (echo "Job already submitted: ${job}" && continue)
+        cp $job /workspace/dtp-jobs/$dir/$jobname
+        submit-job.sh $dir $job
+    done
 done
+    
